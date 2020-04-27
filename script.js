5bcc4f0761a9359aec2f173c8c6f3b1e927574b8
Jamie Thompson <jlthompson76@users.noreply.github.com>
2020-04-27 16:59:07 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,109 @@
+// PART 1
+// Select submit button and call it submitButton
+
+// Select reset button and call it resetButton
+
+// Select main content area and call it main
+
+// Select how to use element and call it howToUse
+
+// Create an empty array for all heroes and call it heroes
+
+// Create an empty string for all cards of heroes and call it cards
+
+
+
+
+// PART 3
+// Create a function called createHeroesArray that takes in an event object
+
+    // Prevent the button from doing what it naturally does (redirect to a new page)
+    
+
+    // Create a Hero constructor that takes in - name, quote, imgURL
+    
+
+    // Create a new Hero object that stores the values of each input field (name, quote, imgURL)
+    
+
+    // Push the new object into the heroes array
+    
+
+    // call the createCard function (you will make this shortly)
+    
+
+    // call the addHero function (you will make this shortly)
+    
+
+
+
+// PART 4
+// Create a function called createCard
+
+    // create a loop to go through the heroes array
+   
+        // CREATE A CARD
+        //create elements for div.card, h3, img, p
+        
+
+        // add a class called hero-card to div.card element
+        
+
+        // add a class called heroImg to the img element
+        
+
+        // set attribute src to the img element with the imgURL from the heroes array
+        
+
+        // create textnode with the hero's name and append it into the h3 element
+        
+
+        // create textnode with the hero's quote and append into the p element
+       
+
+        // append h3 to card
+        
+
+        // apend img to card
+        
+
+        // apend p to card
+        
+
+        // append card to main
+ 
+
+
+// PART 5
+// create a function called addHero
+
+    // clear the main div of any content
+    
+
+    // call the createCard function
+ 
+
+
+// PART 7
+// create a function called resetForm 
+
+    // select the form tag and use the reset method to clear the content
+ 
+
+
+// PART 8
+// create a function called howToUsePopup that takes in an event object
+
+    // Prevent the button from doing what it naturally does (redirect to a new page)
+  
+
+    // make an alert that will provide the following instructions "Fill out the form below to add a new hero onto the main section"
+ 
+
+
+// PART 2
+//create an event listener to the following:
+    //submitButton = click, createHeroesArray
+    // resetButton = click, resetForm
+    // howToUse = click, howToUsePopup
+
